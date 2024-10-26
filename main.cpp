7c2725b7f4a5747a3376f86f8a8adf2d329badfb
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-10-25 18:18:05 -0700

    10 minute timestamp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*
+    OBJECTIVES
+    Task One: See Goat.h
+    Task Two: Write a GM3K1 engine that loops on this menu.
+        Write a main_menu() function that outputs this and then obtains, validates, and returns the user's choice.
+*/
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -9,11 +16,12 @@ const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
 
 int select_goat(list<Goat> trip);
 void delete_goat(list<Goat> &trip);
-void add_goat(list<Goat> &trip, string [], string []);
+void add_goat(list<Goat> &trip, string[], string[]);
 void display_trip(list<Goat> trip);
 int main_menu();
 
-int main() {
+int main()
+{
     srand(time(0));
     bool again;
 
@@ -21,17 +29,32 @@ int main() {
     ifstream fin("names.txt");
     string names[SZ_NAMES];
     int i = 0;
-    while (fin >> names[i++]);
+    while (fin >> names[i++])
+        ;
     fin.close();
     ifstream fin1("colors.txt");
     string colors[SZ_COLORS];
     i = 0;
-    while (fin1 >> colors[i++]);
+    while (fin1 >> colors[i++])
+        ;
     fin1.close();
 
-
-
+    main_menu();
 
     return 0;
 }
 
+// main menu fuction for Task 2
+int main_menu()
+{
+
+    // output prompt
+    cout << "*** GOAT MANAGER 3001 ***" << endl
+        << "[1] Add a goat" << endl
+        << "[2] Delete a goat" << endl
+        << "[3] List goats" << endl
+        << "[4] Quit" << endl
+        << "Choice --> ";
+
+    return 0;
+}
